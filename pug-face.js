4c227a495c6c8b36cb72dbf301668bc59fd3aa94
Mike Atkins <apeherder@gmail.com>
2014-11-28 19:10:04 -0500

    It works!!!!!

--- pug-face.js
+++ pug-face.js
@@ -1,19 +1,81 @@
-var ccv = require('./ccv');
-var cascade = require('./cascade');
+var querystring = require('querystring');
+var hyperquest = require('hyperquest');
+var bl = require('bl');
+var async = require('async');
+var to_array = require('to-array');
+var xtend = require('xtend');
+var pick = require('lodash.pick');
 
-window.onload = function() {
-  var img = document.createElement("img");
-  img.setAttribute("width", "100px");
-  img.setAttribute("height", "100px");
-  img.setAttribute("src", "/faces.jpg");
-  img.setAttribute("id", "pug-face");
+function generate_position(face) {
+  var img_width = face.img_width;
+  var img_height = face.img_height;
+  var width = face.position.width * .01 * img_width;
+  var height = face.position.height * .01 * img_height;
+  var center_x = face.position.center.x * .01 * img_width;
+  var center_y = face.position.center.y * .01 * img_height;
 
-  var comp = ccv.detect_objects({ 
-    canvas : ccv.grayscale(ccv.pre(img)),
-    cascade: cascade,
-    interval: 4,
-    min_neighbors: 1
+  return {
+    width: width,
+    height: height,
+    top: center_y - height / 2,
+    left: center_x - width / 2
+  }
+}
+
+function place_pug(img, faces) {
+  var div = document.createElement('div');
+  div.style.position = 'relative';
+  div.style.display = 'inline';
+  div.appendChild(img);
+
+  faces.face.forEach(function(face) {
+    var position = generate_position(
+      xtend(face, pick(faces, 'img_width', 'img_height'))
+    );
+    var pug = document.createElement('img');
+    pug.style.position = 'absolute';
+    pug.style.width = position.width;
+    pug.style.height = position.height;
+    pug.style.top = position.top;
+    pug.style.left = position.left;
+    pug.src = '/pug-face.png';
+
+    div.appendChild(pug);
+  });
+  document.body.appendChild(div);
+}
+
+function get_faces(img_url, cb) {
+  var qs = querystring.stringify({
+    url: img_url,
+    mode: 'normal',
+    api_key: '4571281cef0e8bb9f09b51435e767d9c',
+    api_secret: 'OWwpfDvlLF1AAOGGbwSEcnkDdZrikNkL'
   });
 
-  console.log(comp);
+  hyperquest.get(
+    'http://apius.faceplusplus.com/v2/detection/detect?'+qs,
+    {
+      withCredentials: false
+    }
+  ).
+  pipe(bl(function(err, buff) {
+    if(err) { cb(err) }
+    else {
+      var body = buff.toString();
+      var faces = JSON.parse(body);
+      cb(null, faces);
+    }
+  }));
+}
+
+window.onload = function() {
+  var images = to_array(document.images);
+
+  images.forEach(function(image) {
+    get_faces(image.src, function(err, faces) {
+      if(err) { console.err(err); }
+      else { place_pug(image, faces); }
+    });
+  });
 };
